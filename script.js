6dfe9ca89e551c2c9a5031843fb83b77545e6491
prasantamang <prashantamang059@gmail.com>
2021-06-01 19:43:50 +0545

    newfile

---
+++ script.js
@@ -0,0 +1,5 @@
+const secondInput = document. createElement('input');
+document.body.appendChild(secondInput);
+const fourthInput = document .createElement('button');
+document.body.appendChild(fourthInput);
+fourthInput.innerHTML ="BUTTON"
